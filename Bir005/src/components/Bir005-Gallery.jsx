6481724386c379089f3872004c7cb4f5d0a1aa7a
Ashath19 <97071508+Ashath19@users.noreply.github.com>
2024-10-08 07:45:12 +0530

    Gallery updated with a sliding animation.

--- Bir005/src/components/Bir005-Gallery.jsx
+++ Bir005/src/components/Bir005-Gallery.jsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from 'react';
 import '../App.css'
 import './Bir005-Gallery.css'
 import family from '../assets/img/Bir005-Gallery/family.png'
+import group from '../assets/img/Bir005-Gallery/groupphotos.png'
+import single from '../assets/img/Bir005-Gallery/singlephotos.png'
+
+const photos = [
+  { src: family, text: 'FAMILY PHOTOS' },
+  { src: group, text: 'GROUP PHOTOS' },
+  { src: single, text: 'SINGLE PHOTOS' }
+];
 function Bir005_Gallery() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % photos.length);
+    }, 4000); // Change photo every 2 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, []);
   return (
     <>
       <section id='bir005-Gallery'>
         <div className='nimbus-container'>
-          <div className='bir005-gal-head'>
-            <h1>Gallery</h1>
+          <h1 className='bir005-gal-head'>Gallery</h1>
+        <div
+        className="bir005-gallery-slider"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {photos.map((photo, index) => (
+          <div key={index} className="bir005-gallery-slide">
             <div className='bir005-familypic'>
-              <img src={family} alt="" />
+              <img src={photo.src} alt={`Family ${index + 1}`} />
               <div className="bir005-picbutton">
-                <span>FAMILY PHOTOS</span>
+                <span>{photo.text}</span>
               </div>
             </div>
           </div>
+        ))}
+      </div>
         </div>
       </section>
     </>
@@ -22,3 +47,16 @@ function Bir005_Gallery() {
 }
 
 export default Bir005_Gallery
+
+
+
+
+{/* <div >
+            <h1>Gallery</h1>
+            <div className='bir005-familypic'>
+              <img src={family} alt="" />
+              <div className="bir005-picbutton">
+                <span>FAMILY PHOTOS</span>
+              </div>
+            </div>
+          </div> */}
